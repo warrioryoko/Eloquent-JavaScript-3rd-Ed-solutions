ec13b40743d4c84a13f7059286f5e79e53c3a093
Ron Yonker <miangpellegri@gmail.com>
2018-03-13 13:20:25 -0500

    Simple solution for looping a triangle

---
+++ LoopingTriangle.js
@@ -0,0 +1,9 @@
+/*Written by Ron Yonker, Tuesday, March 13th 2018*/
+
+/*Simplistic. Many ways to write this, I chose a for loop, keeping in mind I might want to refactor this later to inject this into an HTML DOM node later.*/
+
+var output = '';
+var rows = 7;
+for (var i = 0; i < rows; i++) {
+  console.log(output += '#');
+}
