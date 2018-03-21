987807ebab5c64c87abd5098f6dfaf4f97794469
warrioryoko <miangpellegri@gmail.com>
2018-03-21 15:11:38 -0500

    Oops. Using TextEdit on OS X to write my code instead of VS Code was CLEARLY a mistake.

--- Chapter 3/Minimum.js
+++ Chapter 3/Minimum.js
@@ -1,12 +1,5 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+/*Written by Ron Yonker, Wednesday, March 21st 2018*/
 
-\f0\fs24 \cf0 /*Written by Ron Yonker, Wednesday, March 21st 2018*/\
-\
-function min(x,y) \{\
-  return x<= y ? x : y; /*Used ternary operator to replace \'91if/else\'92; if x is less than or equal to y, return it. Otherwise, return y*/\
-\}\
+function min(x,y) {
+  return x<= y ? x : y; /*Used ternary operator to replace \'91if/else\'92; if x is less than or equal to y, return it. Otherwise, return y*/
 }
