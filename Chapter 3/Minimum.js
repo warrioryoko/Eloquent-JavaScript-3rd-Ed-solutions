c8f56c6bdf4eb3cd3b6bfb31bb1cfa5ee8604e2d
warrioryoko <miangpellegri@gmail.com>
2018-03-23 00:08:33 -0500

    Removed extraneous characters

--- Chapter 3/Minimum.js
+++ Chapter 3/Minimum.js
@@ -1,5 +1,5 @@
 /*Written by Ron Yonker, Wednesday, March 21st 2018*/
 
 function min(x,y) {
-  return x<= y ? x : y; /*Used ternary operator to replace \'91if/else\'92; if x is less than or equal to y, return it. Otherwise, return y*/
+  return x<= y ? x : y; /*Used ternary operator to replace 'if/else'; if x is less than or equal to y, return it. Otherwise, return y*/
 }
